d303182f786df1a076177733623e9e5c3d00bbad
Isaraa <isaraa.p@ku.th>
2025-03-19 18:30:03 +0700

    adjust matching page

--- mingle/minglev2_1/lib/Screen/match_menu_page.dart
+++ mingle/minglev2_1/lib/Screen/match_menu_page.dart
@@ -18,10 +18,34 @@ class _FindMatchPageState extends State<FindMatchPage> {
   DateTime? selectedDateTime;
   double distance = 1.0;
 
+  // Map activities to their respective icons
+  final Map<String, IconData> activityIcons = {
+    'Hiking': Icons.directions_walk,
+    'Reading': Icons.menu_book,
+    'Cooking': Icons.restaurant,
+    'Traveling': Icons.flight_takeoff,
+    'Gaming': Icons.videogame_asset,
+    'Yoga': Icons.self_improvement,
+    'Dancing': Icons.music_note,
+    'Swimming': Icons.pool,
+    'Cycling': Icons.directions_bike,
+    'Running': Icons.directions_run,
+    'Painting': Icons.palette,
+    'Photography': Icons.camera_alt,
+    'Eating': Icons.fastfood,
+    'Karaoke': Icons.mic,
+    'Shopping': Icons.shopping_cart,
+    'Watching Movies': Icons.movie,
+    'Gym': Icons.fitness_center,
+    'Coffee': Icons.coffee,
+  };
+
   final List<String> activityOptions = [
     'Hiking', 'Reading', 'Cooking', 'Traveling',
     'Gaming', 'Yoga', 'Dancing', 'Swimming',
-    'Cycling', 'Running', 'Painting', 'Photography'
+    'Cycling', 'Running', 'Painting', 'Photography',
+    'Eating', 'Karaoke', 'Shopping', 'Watching Movies',
+    'Gym', 'Coffee',
   ];
 
   Future<void> _selectDateTime(BuildContext context) async {
@@ -61,14 +85,15 @@ class _FindMatchPageState extends State<FindMatchPage> {
           children: [
             Text(
               'Find Your Activity Partner',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
             ),
             Text(
               'Customize your preferences to find the perfect activity buddy.',
-              style: TextStyle(fontSize: 16, color: Colors.black),
+              style: TextStyle(fontSize: 14, color: Colors.white70),
             ),
           ],
         ),
+        backgroundColor: Colors.blue,
       ),
       bottomNavigationBar: CustomBottomNavBar(
         currentIndex: currentPageIndex,
@@ -89,158 +114,187 @@ class _FindMatchPageState extends State<FindMatchPage> {
           }
         },
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Gender Section
-            Text(
-              'Gender',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),
-            ),
-            SizedBox(height: 10),
-            Center(
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: DropdownButton<String>(
-                  value: selectedGender,
-                  hint: Text(
-                    'Choose gender',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      selectedGender = newValue;
-                    });
-                  },
-                  items: <String>['Male', 'Female', 'Other'].map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(
-                        value,
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    );
-                  }).toList(),
-                  underline: Container(),
-                ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue.shade100, Colors.white],
+          ),
+        ),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Gender Section
+              Text(
+                'Select Gender',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87),
               ),
-            ),
-            SizedBox(height: 20),
-
-            // Activity Section
-            Text(
-              'Activities',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),
-            ),
-            SizedBox(height: 10),
-            Center(
-              child: Wrap(
-                spacing: 8.0,
-                runSpacing: 8.0,
-                alignment: WrapAlignment.center,
-                children: activityOptions.map((String activity) {
-                  return ChoiceChip(
-                    label: Text(
-                      activity,
-                      style: TextStyle(
-                        color: selectedActivities.contains(activity) ? Colors.white : Colors.black),
-                      ),
-                    selected: selectedActivities.contains(activity),
-                    onSelected: (bool selected) {
-                      setState(() {
-                        if (selected && selectedActivities.length < 3) {
-                          selectedActivities.add(activity);
-                        } else {
-                          selectedActivities.remove(activity);
-                        }
-                      });
-                    },
-                    selectedColor: Colors.blue,
-                  );
-                }).toList(),
-              ),
-            ),
-            SizedBox(height: 20),
-
-            // DateTime Section
-            Text(
-              'Date and Time',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),
-            ),
-            SizedBox(height: 10),
-            Center(
-              child: InkWell(
-                onTap: () => _selectDateTime(context),
+              SizedBox(height: 10),
+              Center(
                 child: Container(
-                  padding: EdgeInsets.all(12),
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
                   decoration: BoxDecoration(
+                    color: Colors.white,
                     border: Border.all(color: Colors.grey),
                     borderRadius: BorderRadius.circular(8),
                   ),
-                  child: Text(
-                    selectedDateTime == null ? 'Click to select a date and time' : 'Selected Date and Time: ${selectedDateTime!.day}/${selectedDateTime!.month}/${selectedDateTime!.year} at ${selectedDateTime!.hour}:${selectedDateTime!.minute}',
-                    style: TextStyle(fontSize: 14, color: Colors.black),
+                  child: DropdownButton<String>(
+                    value: selectedGender,
+                    hint: Text(
+                      'Choose gender',
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedGender = newValue;
+                      });
+                    },
+                    items: <String>['Male', 'Female', 'Other'].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      );
+                    }).toList(),
+                    underline: Container(),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
+              SizedBox(height: 20),
 
-            // Distance Section
-            Text(
-              'Distance',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),
-            ),
-            SizedBox(height: 10),
-            Center(
-              child: Column(
-                children: [
-                  Text(
-                    '${distance.round()} km',
-                    style: TextStyle(fontSize: 22, color: Colors.black),
-                  ),
-                  Slider(
-                    value: distance,
-                    min: 1,
-                    max: 20,
-                    divisions: 19,
-                    onChanged: (double value) {
-                      setState(() {
-                        distance = value;
-                      });
-                    },
-                    activeColor: Colors.blue,
-                  ),
-                ],
+              // Activity Section
+              Text(
+                'Select Activities (up to 3)',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87),
               ),
-            ),
-            SizedBox(height: 20),
-          ],
+              SizedBox(height: 10),
+              Center(
+                child: Wrap(
+                  spacing: 8.0,
+                  runSpacing: 8.0,
+                  alignment: WrapAlignment.center,
+                  children: activityOptions.map((String activity) {
+                    return ChoiceChip(
+                      label: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            activityIcons[activity],
+                            size: 20,
+                            color: selectedActivities.contains(activity) ? Colors.white : Colors.black,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            activity,
+                            style: TextStyle(
+                              color: selectedActivities.contains(activity) ? Colors.white : Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      selected: selectedActivities.contains(activity),
+                      onSelected: (bool selected) {
+                        setState(() {
+                          if (selected && selectedActivities.length < 3) {
+                            selectedActivities.add(activity);
+                          } else {
+                            selectedActivities.remove(activity);
+                          }
+                        });
+                      },
+                      selectedColor: Colors.blue,
+                      backgroundColor: Colors.grey[200],
+                    );
+                  }).toList(),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              // DateTime Section
+              Text(
+                'Select Date and Time',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87),
+              ),
+              SizedBox(height: 10),
+              Center(
+                child: InkWell(
+                  onTap: () => _selectDateTime(context),
+                  child: Container(
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Text(
+                      selectedDateTime == null ? 'Click to select a date and time' : 'Selected Date and Time: ${selectedDateTime!.day}/${selectedDateTime!.month}/${selectedDateTime!.year} at ${selectedDateTime!.hour}:${selectedDateTime!.minute}',
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              // Distance Section
+              Text(
+                'Set Distance (in km)',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87),
+              ),
+              SizedBox(height: 10),
+              Center(
+                child: Column(
+                  children: [
+                    Text(
+                      '${distance.round()} km',
+                      style: TextStyle(fontSize: 22, color: Colors.black),
+                    ),
+                    Slider(
+                      value: distance,
+                      min: 1,
+                      max: 20,
+                      divisions: 19,
+                      onChanged: (double value) {
+                        setState(() {
+                          distance = value;
+                        });
+                      },
+                      activeColor: Colors.blue,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 80), // Add extra padding at the bottom to avoid overlapping
+            ],
+          ),
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: ElevatedButton(
-        onPressed: () {
-           Navigator.of(context).pushReplacement(
+      floatingActionButton: Container(
+        margin: EdgeInsets.only(bottom: 20), // Add margin to avoid overlapping with the bottom navigation bar
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacement(
               MaterialPageRoute(builder: (context) => SearchingPage()),
             );
-        },
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blue,
-          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-        ),
-        child: Text(
-          'Find Matches',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+          child: Text(
+            'Find Matches',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
         ),
       ),
     );
   }
 }
-
